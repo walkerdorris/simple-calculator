da64dd71e8ffc7f062cdbd7378e754f43bd841a1
Walker Dorris <walker.dorris@gmail.com>
2016-03-01 19:31:21 -0800

    basic setup plus link to expression and test

---
+++ simple-calculator/ConsoleApplication1/Expression.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simplecalculator
+{
+    public class Expression
+    {
+
+        public int Max { get; private set; }
+        protected int Step { get; set; }
+        protected int First { get; set; }
+
+       
+
+       
+
+        
+    }
+}
