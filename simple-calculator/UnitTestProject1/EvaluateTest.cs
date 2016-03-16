bcb2486658fb4347dfb561f0fb9eb1dc842c9e5e
Walker Dorris <walker.dorris@gmail.com>
2016-03-15 19:41:22 -0700

    chunk 2 complete

--- simple-calculator/UnitTestProject1/EvaluateTest.cs
+++ simple-calculator/UnitTestProject1/EvaluateTest.cs
@@ -20,7 +20,63 @@ namespace EvaluateTests
             char Oper = '+';
 
             int answer = input.Calculate(1, 2, '+');
-            Assert.Equals(3, answer);
+            Assert.AreEqual(3, answer);
+        }
+
+        [TestMethod]
+        public void SubtractionTest()
+        {
+            Evaluate input = new Evaluate();
+            int Term1 = 4;
+            int Term2 = 2;
+            char Oper = '-';
+
+            int answer = input.Calculate(4, 2, '-');
+            Assert.AreEqual(2, answer);
+        }
+        [TestMethod]
+        public void MultiplicationTest()
+        {
+            Evaluate input = new Evaluate();
+            int Term1 = 1;
+            int Term2 = 2;
+            char Oper = '*';
+
+            int answer = input.Calculate(1, 2, '*');
+            Assert.AreEqual(2, answer);
+        }
+        [TestMethod]
+        public void DivisionTest()
+        {
+            Evaluate input = new Evaluate();
+            int Term1 = 4;
+            int Term2 = 2;
+            char Oper = '/';
+
+            int answer = input.Calculate(4, 2, '/');
+            Assert.AreEqual(2, answer);
+        }
+        [TestMethod]
+        public void ModulusTest()
+        {
+            Evaluate input = new Evaluate();
+            int Term1 = 5;
+            int Term2 = 2;
+            char Oper = '%';
+
+            int answer = input.Calculate(5, 2, '%');
+            Assert.AreEqual(1, answer);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void EvaluateExceptionTest()
+        {
+            Evaluate input = new Evaluate();
+            int Term1 = 1;
+            int Term2 = 2;
+            char Oper = '7';
+
+            int answer = input.Calculate(1, 2, '7');
         }
     }
 }
